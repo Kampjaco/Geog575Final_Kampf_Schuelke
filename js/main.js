5ca4cbdf37e58d3e8dc78e0187b002ff575e82fe
Jacob Kampf <jkampf@wisc.edu>
2025-04-23 17:40:42 -0500

    Put pop csv into data folder.  Working on getting map into page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,52 @@
-//insert code here!
+//Executes when the window loads
+window.onload = function () {
+    //Semi global variables
+
+    
+    //////////////////////////////////////////////
+    /////////////////////////////////////////////
+    // MAP JAVASCRIPT //
+
+    initializeMap();
+
+    function initializeMap() {
+
+        var map = L.map('map').setView([44.54746726659841, -89.96017005851391], 7);
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
